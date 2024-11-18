3a81cc80d84d58923bfa821b1af6fb3c3bf71b97
Thilankavishka <139649893+Thilankavishka@users.noreply.github.com>
2024-11-18 14:27:34 +0000

    commit project student information system

---
+++ studentapp/src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import TableCreate from "./components/table";
+import "./assets/css/compo.css";
+import { students } from "./data/StudentsDb";
+import { useState } from "react";
+
+function App() {
+  const [fontSize, setFontSize] = useState("16px");
+
+  const changeFontSize = (size) => {
+    switch (size) {
+      case "small":
+        setFontSize("16px");
+        break;
+      case "medium":
+        setFontSize("20px");
+        break;
+      case "large":
+        setFontSize("24px");
+        break;
+      default:
+        setFontSize("16px");
+    }
+  };
+  return (
+    <>
+      <div style={{ fontSize }}>
+        <div>
+          <h3>Font Size</h3>
+          <button onClick={() => changeFontSize("small")}>S</button>
+          <button onClick={() => changeFontSize("medium")}>M</button>
+          <button onClick={() => changeFontSize("large")}>L</button>
+        </div>
+        <tr></tr>
+        <div>
+          <TableCreate students={students}></TableCreate>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
