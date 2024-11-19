5f9fdc80087c12bb66779e0d70f151b6bb8cec91
Thilankavishka <139649893+Thilankavishka@users.noreply.github.com>
2024-11-19 08:28:07 +0000

    Create UseState File in App.js file

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -7,36 +7,10 @@ import { useState } from "react";
 function App() {
   const [fontSize, setFontSize] = useState("16px");
 
-  const changeFontSize = (size) => {
-    switch (size) {
-      case "small":
-        setFontSize("16px");
-        break;
-      case "medium":
-        setFontSize("20px");
-        break;
-      case "large":
-        setFontSize("24px");
-        break;
-      default:
-        setFontSize("16px");
-    }
-  };
+ 
   return (
     <>
-      <div style={{ fontSize }}>
-        <div>
-          <h3>Font Size</h3>
-          <button onClick={() => changeFontSize("small")}>S</button>
-          <button onClick={() => changeFontSize("medium")}>M</button>
-          <button onClick={() => changeFontSize("large")}>L</button>
-        </div>
-        <tr></tr>
-        <div>
-          <TableCreate students={students}></TableCreate>
-        </div>
-        <div></div>
-      </div>
+      
     </>
   );
 }
