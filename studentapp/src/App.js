a36cb49ae0de397588aa8dfae10b42280481a72c
Thilankavishka <139649893+Thilankavishka@users.noreply.github.com>
2024-11-19 08:28:41 +0000

    Create font size Creation switch case

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -6,7 +6,21 @@ import { useState } from "react";
 
 function App() {
   const [fontSize, setFontSize] = useState("16px");
-
+  const changeFontSize = (size) => {
+    switch (size) {
+      case "small":
+        setFontSize("16px");
+        break;
+      case "medium":
+        setFontSize("20px");
+        break;
+      case "large":
+        setFontSize("24px");
+        break;
+      default:
+        setFontSize("16px");
+    }
+  };
  
   return (
     <>
