9e8f8a730fa82ceb27490e3b339e66510d3d93b1
Thilankavishka <139649893+Thilankavishka@users.noreply.github.com>
2024-11-19 08:29:18 +0000

    Add Components to App.js file

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -24,8 +24,20 @@ function App() {
  
   return (
     <>
-      
-    </>
+    <div style={{ fontSize }}>
+      <div>
+        <h3>Font Size</h3>
+        <button onClick={() => changeFontSize("small")}>S</button>
+        <button onClick={() => changeFontSize("medium")}>M</button>
+        <button onClick={() => changeFontSize("large")}>L</button>
+      </div>
+      <tr></tr>
+      <div>
+        <TableCreate students={students}></TableCreate>
+      </div>
+      <div></div>
+    </div>
+  </>
   );
 }
 
